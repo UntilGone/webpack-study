dfdcda1b5c261d6284fc20be6963be14d6cfcd0a
UntilGone <t_vampire@sina.cn>
2022-07-27 15:01:18 +0800

    feat: 添加eslint配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -9,19 +10,13 @@ module.exports = {
   },
   module: {
     rules: [
-      // 使用ts loader处理ts
-      // 相比较babel  ts-loader会在编译过程中进行类型校验 代价是更长的编译时间
       {
-        test: /\.ts$/, // 处理ts文件
-        use: 'ts-loader',
-      },
-      {
-        test: /\.js$/, // 只处理ts js文件
+        test: /\.(ts)|(js)$/, // 只处理ts js文件
         use: [
           {
             loader: 'babel-loader', // 使用babel-loader
             options: {
-              presets: ['@babel/preset-env'], // babel配置  常见的有@babel/preset-env @babel/preset-typescript babel-preset-react等
+              presets: ['@babel/preset-env', '@babel/preset-typescript'], // babel配置  常见的有@babel/preset-env @babel/preset-typescript babel-preset-react等
             },
           },
         ],
@@ -31,4 +26,9 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.js'], // 声明自动解析 当import './a' 会按顺序找a.ts a.js
   },
+  plugins: [
+    new ESLintPlugin({
+      extensions: ['.ts', '.js'],
+    }),
+  ],
 };
