e20bbc8b16710ae3541663edff3b5f12d9b85863
UntilGone <t_vampire@sina.cn>
2022-07-27 14:20:56 +0800

    feat: ts-loader和babel两套配

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 const sum = (a: number, b: number) => a + b;
-const num1 = '5';
+const num1 = '5'; // num1 is string not a number
 const num2 = 7;
+// function sum should be error because num1 is not a number
 console.log(`${num1} add ${num2} is`, sum(num1, num2));
--- webpack.config.js
+++ webpack.config.js
@@ -11,17 +11,19 @@ module.exports = {
     rules: [
       // 使用ts loader处理ts
       // 相比较babel  ts-loader会在编译过程中进行类型校验 代价是更长的编译时间
+      // index.ts中有类型错误  如果使用这个配置就会编译报错 用时1473ms
+      // {
+      //   test: /\.ts$/, // 处理ts文件
+      //   use: 'ts-loader',
+      // },
+      // 用babel则不会报错 babel只做代码转换 用时692ms
       {
-        test: /\.ts$/, // 处理ts文件
-        use: 'ts-loader',
-      },
-      {
-        test: /\.js$/, // 只处理ts js文件
+        test: /\.(ts)|(js)$/, // 只处理ts js文件
         use: [
           {
             loader: 'babel-loader', // 使用babel-loader
             options: {
-              presets: ['@babel/preset-env'], // babel配置  常见的有@babel/preset-env @babel/preset-typescript babel-preset-react等
+              presets: ['@babel/preset-typescript'], // babel配置  常见的有@babel/preset-env @babel/preset-typescript babel-preset-react等
             },
           },
         ],
